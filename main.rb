ea818dc2b3f7a25d708f9a95e7d8f137389b0bfd
Ian Lam <ljy200523@gmail.com>
2026-04-23 15:55:11 +0800

    Edit place() and initialize
    place(position) changed to place(row, col) to access array,
    unless using splatting to destructure position
    Merge initialize and space() - supposed to be initialize only

--- main.rb
+++ main.rb
@@ -2,15 +2,16 @@ class Players
   attr_accessor :name
   def initialize(name)
     @name = name
-  end
+  end  
 end
 
 class Table
-  attr_accessor :space
+  attr_accessor :space, :place
   def initialize
+    space = Array.new(3) {Array.new(3, "placeholder")} 
   end
-  def space()
-    space = Array.new(3) {Array.new(3, "tired")}
+  def place(player, row, col)
+    space[row][col] = player
   end
   def self.check_win()
     three_in_a_row ? true : false
@@ -20,4 +21,5 @@ end
 player_one = Players.new("X")
 puts player_one.name
 table = Table.new
+table.place("X", 1, 2)
 p table.space
